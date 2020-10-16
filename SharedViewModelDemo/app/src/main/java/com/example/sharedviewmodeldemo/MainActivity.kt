6a15f17d356fba59f87cf4b45ae734c89063bc87
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-10-15 22:49:22 -0400

    Added Source and Target fragments

---
+++ SharedViewModelDemo/app/src/main/java/com/example/sharedviewmodeldemo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.sharedviewmodeldemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Plant tree to enable Debugging with Timber
+        Timber.plant(Timber.DebugTree())
+
+    }
+}
---
+++ SharedViewModelDemo/app/src/main/java/com/example/sharedviewmodeldemo/ui/destination_fragment/DestinationFragment.kt
@@ -0,0 +1,2 @@
+package com.example.sharedviewmodeldemo.ui.destination_fragment
+
