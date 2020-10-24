ac197e987356d7de8ac67342ecc892d0cfd21653
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-10-24 14:03:30 -0400

    Passed data after running the ViewModelProvider through the activity

--- SharedViewModelDemo/app/src/main/java/com/example/sharedviewmodeldemo/ui/source_fragment/SourceFragment.kt
+++ SharedViewModelDemo/app/src/main/java/com/example/sharedviewmodeldemo/ui/source_fragment/SourceFragment.kt
@@ -6,17 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.example.sharedviewmodeldemo.R
 import com.example.sharedviewmodeldemo.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_source.*
+import kotlinx.android.synthetic.main.fragment_target.*
 
 class SourceFragment  : Fragment() {
 
     lateinit var viewModel: SharedViewModel
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_source, container, false)
     }
@@ -24,19 +26,21 @@ class SourceFragment  : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel = SharedViewModel()
+        bt_source_send.setOnClickListener{
+
+            viewModel.setText(et_source.text.toString())
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = activity?.run {
+            ViewModelProvider(this).get(SharedViewModel::class.java)
+        } ?: throw Exception("Invalid Activity")
 
         viewModel.textToSend.observe(viewLifecycleOwner, Observer {text ->
-            if (viewModel.isSent){
-                et_source.setText(text)
-                viewModel.isSent = false
-            }
+            et_source.setText(text)
         })
 
-        bt_source_send.setOnClickListener{
-            viewModel.isSent = true
-            viewModel.setText(et_source.text.toString())
-            //et_source.text.clear()
-        }
     }
 }
--- SharedViewModelDemo/app/src/main/java/com/example/sharedviewmodeldemo/ui/target_fragment/TargetFragment.kt
+++ SharedViewModelDemo/app/src/main/java/com/example/sharedviewmodeldemo/ui/target_fragment/TargetFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.sharedviewmodeldemo.R
 import com.example.sharedviewmodeldemo.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_source.*
@@ -14,7 +15,7 @@ import timber.log.Timber
 
 class TargetFragment  : Fragment() {
 
-    lateinit var sharedViewModel: SharedViewModel
+    lateinit var viewModel: SharedViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,19 +26,19 @@ class TargetFragment  : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val sharedViewModel = SharedViewModel()
-
-        sharedViewModel.textToSend.observe(viewLifecycleOwner, Observer {text ->
-            if (sharedViewModel.isSent){
-                et_target.setText(text)
-                sharedViewModel.isSent = false
-            }
-        })
-
         bt_target_send.setOnClickListener{
-            sharedViewModel.isSent = true
-            sharedViewModel.setText(et_target.text.toString())
-            //et_target.text.clear()
+            viewModel.setText(et_target.text.toString())
         }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = activity?.run {
+            ViewModelProvider(this).get(SharedViewModel::class.java)
+        } ?: throw Exception("Invalid Activity")
+
+        viewModel.textToSend.observe(viewLifecycleOwner, Observer {text ->
+            et_target.setText(text)
+        })
     }
 }
