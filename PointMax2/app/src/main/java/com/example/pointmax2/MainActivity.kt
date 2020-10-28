8ddcaf5587e59e82a9296bf46dfb2ff3b4052111
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-10-27 22:11:07 -0400

    Bottom bar now correctly navigates to correct fragment

--- PointMax2/app/src/main/java/com/example/pointmax2/MainActivity.kt
+++ PointMax2/app/src/main/java/com/example/pointmax2/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.pointmax2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +16,21 @@ class MainActivity : AppCompatActivity() {
 
         // Plant tree to enable Debugging with Timber
         Timber.plant(Timber.DebugTree())
+
+        // Find the bottomNavigation bar
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        // Find the fragment that will host the different fragments
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_wallet
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
