f335f538e835436b2521abf11543f206e04a2b2d
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-10-27 21:53:00 -0400

    Adding bottom bar and floating action button

--- PointMax2/app/src/main/java/com/example/pointmax2/MainActivity.kt
+++ PointMax2/app/src/main/java/com/example/pointmax2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.pointmax2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Plant tree to enable Debugging with Timber
+        Timber.plant(Timber.DebugTree())
     }
 }
