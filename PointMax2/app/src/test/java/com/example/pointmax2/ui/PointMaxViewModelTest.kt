43b7de77627cd6c0bbf0338ab2477cb81d8a4832
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-11-08 12:56:11 -0500

    * Added a ViewModel test

---
+++ PointMax2/app/src/test/java/com/example/pointmax2/ui/PointMaxViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.example.pointmax2.ui
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class PointMaxViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
