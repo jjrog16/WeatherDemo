3f9a476c3030adee8f28ad6334e608eb5e1e93c8
jjrog16 <52580893+jjrog16@users.noreply.github.com>
2020-12-24 16:26:54 -0500

    Updated RetrofitInstance.kt to include additional comments

--- WeatherDemo/app/src/main/java/com/example/api/RetrofitInstance.kt
+++ WeatherDemo/app/src/main/java/com/example/api/RetrofitInstance.kt
@@ -1,11 +1,33 @@
 package com.example.api
 
+import com.example.data.remote.Weather
+import com.example.util.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object{
         private val retrofit by lazy {
+            // Used for additional Debugging of HTTP requests
             val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build()
+            // Build the Retrofit object
+            Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    // Google defined converter of responses to JSON
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build()
+        }
+
+        // The API object available to use to make network requests
+        val api by lazy {
+            retrofit.create(WeatherAPI::class.java)
         }
     }
 }
--- WeatherDemo/app/src/main/java/com/example/util/Constants.kt
+++ WeatherDemo/app/src/main/java/com/example/util/Constants.kt
@@ -3,5 +3,6 @@ package com.example.util
 class Constants {
     companion object {
         const val API_ID = "1e4104ed74eca5832410744e3d9442e9"
+        const val BASE_URL = "https://api.openweathermap.org"
     }
 }
